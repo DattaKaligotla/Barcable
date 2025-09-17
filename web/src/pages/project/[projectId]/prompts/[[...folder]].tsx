684691625a0bf5c925ac5e6e7a2a4831a9648f60
Datta Kaligotla <dattakaligotla@Dattas-MacBook-Pro.local>
2025-09-17 14:41:30 -0700

    changes

--- web/src/pages/project/[projectId]/prompts/[[...folder]].tsx
+++ web/src/pages/project/[projectId]/prompts/[[...folder]].tsx
@@ -4,7 +4,7 @@ import Page from "@/src/components/layouts/page";
 import { usePostHogClientCapture } from "@/src/features/posthog-analytics/usePostHogClientCapture";
 import { PromptTable } from "@/src/features/prompts/components/prompts-table";
 import { useHasProjectAccess } from "@/src/features/rbac/utils/checkProjectAccess";
-import { PlusIcon, Sparkles, Store, GitBranch, BarChart3 } from "lucide-react";
+import { PlusIcon } from "lucide-react";
 import { api } from "@/src/utils/api";
 import { PromptsOnboarding } from "@/src/components/onboarding/PromptsOnboarding";
 import { useEntitlementLimit } from "@/src/features/entitlements/hooks";
@@ -13,13 +13,6 @@ import PromptMetrics from "./metrics";
 import { useQueryParams, StringParam } from "use-query-params";
 import React from "react";
 import { AutomationButton } from "@/src/features/automations/components/AutomationButton";
-import { Button } from "@/src/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/src/components/ui/dropdown-menu";
 
 export default function PromptsWithFolder() {
   const router = useRouter();
@@ -98,48 +91,6 @@ export default function PromptsWithFolder() {
         actionButtonsRight: (
           <>
             {projectId && <AutomationButton projectId={projectId} />}
-
-            {/* Prompt Engineering Features Dropdown */}
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="sm">
-                  <Sparkles className="mr-2 h-4 w-4" />
-                  Prompt Engineering
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuItem asChild>
-                  <a href={`/project/${projectId}/prompt-engineering`}>
-                    <Sparkles className="mr-2 h-4 w-4" />
-                    Engineering Dashboard
-                  </a>
-                </DropdownMenuItem>
-                <DropdownMenuItem asChild>
-                  <a href={`/project/${projectId}/prompt-variants`}>
-                    <GitBranch className="mr-2 h-4 w-4" />
-                    Prompt Variants
-                  </a>
-                </DropdownMenuItem>
-                <DropdownMenuItem asChild>
-                  <a href={`/project/${projectId}/prompt-store`}>
-                    <Store className="mr-2 h-4 w-4" />
-                    Prompt Store
-                  </a>
-                </DropdownMenuItem>
-                <DropdownMenuItem asChild>
-                  <a href={`/project/${projectId}/prompts/scoring`}>
-                    <BarChart3 className="mr-2 h-4 w-4" />
-                    Scoring Dashboard
-                  </a>
-                </DropdownMenuItem>
-                <DropdownMenuItem asChild>
-                  <a href={`/project/${projectId}/cicd-artifacts`}>
-                    <GitBranch className="mr-2 h-4 w-4" />
-                    CI/CD Artifacts
-                  </a>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
 
             <ActionButton
               icon={<PlusIcon className="h-4 w-4" aria-hidden="true" />}
